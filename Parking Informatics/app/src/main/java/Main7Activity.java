5676fc3db0cda6243528e8a32b5015dd3dc598c1
muddanakeerthana <48143087+muddanakeerthana@users.noreply.github.com>
2020-10-05 21:21:46 +0530

    Update Main7Activity.java

--- Parking Informatics/app/src/main/java/Main7Activity.java
+++ Parking Informatics/app/src/main/java/Main7Activity.java
@@ -1,11 +1,8 @@
 package com.example.keerthana.finaltrial;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class Main7Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
